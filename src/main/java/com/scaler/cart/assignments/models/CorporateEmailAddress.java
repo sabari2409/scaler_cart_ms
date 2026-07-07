70e22582901e10db4084af8532c9342c643da782
Sabari Rajeswaran <sabarirajeswaran2409@gmail.com>
2026-07-07 19:52:32 +0530

    Scaler Backend assignments - Intro to spring JPA - Shared Primary Key

---
+++ src/main/java/com/scaler/cart/assignments/models/CorporateEmailAddress.java
@@ -0,0 +1,30 @@
+package com.scaler.cart.assignments.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+@Entity
+public class CorporateEmailAddress {
+    @Id
+    private Long id;
+
+    private String tenant;
+
+    private Date createdAt;
+
+    @OneToOne
+    @MapsId
+    @JoinColumn(name = "employee_id")
+    private CorporateEmployee employee;
+}
+
+
+/**
+ * You are given with 2 models - CorporateEmployee and CorporateEmailAddress. Fields are already present in these models.
+ * You need to define cardinalities like @ManyToOne, @OneToOne in these models and make sure tables are also created for these classes.
+ * Also instead of creating new column corporate_email_address_id, we want to mark the Primary Key column employee_id of corporateemailaddress table
+ * as the foreign key to corporate_employee table. This way, we will optimize the storage space. This is known as Shared Primary Key.
+ */
